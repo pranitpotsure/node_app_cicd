e1c4f9ff40371fe666c9da0e147ed654506935a0
pranitpotsure <pranitpotsure55@gmail.com>
2025-11-08 11:03:33 +0530

    new files

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+// Import modules
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route
 app.get('/', (req, res) => {
-  res.send('Hello from pranitttt added webhook, this is the third version of node');
+  const appInfo = {
+    version: '3.0',
+    message: 'This is the third version of Node App',
+    time: new Date().toLocaleTimeString()
+  };
+
+  res.render('index', { appInfo });
 });
 
+// Start server
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`🚀 App running at http://localhost:${port}`);
 });
